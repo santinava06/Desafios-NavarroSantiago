7df88f28ef14240b81c9b9911e7264d801632d24
santiagon <sncarp2003@gmail.com>
2022-06-28 20:20:33 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/* Desafio 1 */
+
+for(let index = 1; index < 11; index++){
+    let nombre = prompt("ingrese su nombre");
+    let resultado = `${nombre} eres el cliente nº ${index} de la pagina`;
+    alert(resultado);
+}
+alert("Ya no quedan turnos disponibles, vuelva mañana")
